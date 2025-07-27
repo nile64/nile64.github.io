907731e134e2411db2d7ff07bd3372188737acf9
nile64 <87245996+Nile64@users.noreply.github.com>
2025-07-27 16:34:09 -0400

    i hate

--- js/GDGetStats.js
+++ js/GDGetStats.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Get Profile Stats
     $('#statsErrorText').hide();
     $('#profileStats').hide();
     fetch('https://gdbrowser.com/api/profile/nile64')
@@ -18,5 +19,54 @@ $(document).ready(function(){
             console.error('Error fetching data:', error);
             $('#statsErrorText').show();
             $('#statsLoadingText').hide();
+    });
+
+    // Get extremes.json and use it
+    $.getJSON('/data/extremes.json', function (data) {
+        console.log(data)
+        $('#dRatedExtremes').text(data.ratedExtremes);
+        $('#dUnratedExtremes').text(data.unratedExtremes);
+        $('#dTotalExtremes').text(data.totalExtremes);
+        var lvl = '';
+        
+        $.each(data.extremes, function(i, value) {
+            lvl += '<tr class="demonBG">';
+            lvl += '<td>' + (i + 1) + '</td>';
+            var rate = '';
+            if(value.rating == "unrated"){
+                rate = '<img src="/assets/UnratedExtremeB.png" class="demonDiffFace">'
+            }
+            else if(value.rating == "rate"){
+                rate = '<img src="/assets/RatedExtremeB.png" class="demonDiffFace">'
+            }
+            else if(value.rating == "feature"){
+                rate = '<img src="/assets/FeaturedExtreme.png" class="demonDiffFace">'
+            }
+            else if(value.rating == "epic"){
+                rate = '<img src="/assets/EpicExtreme.png" class="demonDiffFace">'
+            }
+            else if(value.rating == "legendary"){
+                rate = '<img src="/assets/LegendaryExtreme.png" class="demonDiffFace">'
+            }
+            else if(value.rating == "mythic"){
+                rate = '<img src="/assets/MythicExtreme.png" class="demonDiffFace">'
+            }
+
+            if(value.hardest)
+            {
+                lvl += '<td id="' + value.name + '" style="font-size: 30px; font-weight: 700;">' + rate + '<a class="demonLinkHardest" href="' + value.video + '">' + value.name + '</a></td>';
+            }
+            else 
+            {
+                lvl += '<td id="' + value.name + '" style="font-size: 28px; font-weight: 600;">' + rate + '<a class="demonLink" href="' + value.video + '">' + value.name + '</a></td>';
+            }
+            lvl += '<td><a class="idLink" href="https://gdbrowser.com/' + value.id + '">' + value.id + '</td>';
+            lvl += '<td>' + value.attempts + '</td>';
+            lvl += '<td>' + value.date + '</td>';
+            lvl += '</tr>';
+            $('#demonTable').append(lvl);
+            lvl = '';
         });
+    });
+
 });
